05587862fcea70d05284ec2af32e41197ef12873
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2026-05-14 14:58:03 +0900

    ユーザー登録画面の実装

--- trainingspring1/src/main/java/com/example/demo/Service/UserService.java
+++ trainingspring1/src/main/java/com/example/demo/Service/UserService.java
@@ -1,16 +1,20 @@
 package com.example.demo.Service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.Entity.User;
 import com.example.demo.Repository.UserRepository;
+import com.example.demo.dto.UserRequest;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(rollbackFor = Exception.class)
 public class UserService {
 	/**
 	 * ユーザー情報 Repository
@@ -18,9 +22,22 @@ public class UserService {
 	private final UserRepository userRepository;
 
 	public List<User> searchAll() {
-
+		;
 		//ユーザーテーブルの内容を全検索
 		return userRepository.findAll();
+
+	}
+
+	public void create(UserRequest userRequest) {
+
+		Date now = new Date();
+		User user = new User();
+		user.setName(user.getName());
+		user.setAddress(userRequest.getAddress());
+		user.setPhone(userRequest.getPhone());
+		user.setCreateDate(now);
+		user.setUpdateDate(now);
+		userRepository.save(user);
 
 	}
 
--- trainingspring1/src/main/java/com/example/demo/controller/UserController.java
+++ trainingspring1/src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.Entity.User;
 import com.example.demo.Service.UserService;
+import com.example.demo.dto.UserRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -41,6 +48,7 @@ public class UserController {
 
 	@GetMapping(value = "/user/add")
 	public String displayAdd(Model model) {
+		model.addAttribute("userRequest", new UserRequest());
 		return "user./add";
 	}
 
@@ -58,6 +66,33 @@ public class UserController {
 		model.addAttribute("userData", user);
 
 		return "user/view";
+	}
+
+	/**
+	 * ユーザーの新規登録画面
+	 * @param usrRequest リクエストデータ
+	 * @param model Model
+	 * @return ユーザー情報一覧画面
+	 */
+	@PostMapping("/user/create")
+	public String create(@Validated @ModelAttribute UserRequest userRequest, BindingResult result, Model model) {
+
+		//入力チェックエラーの場合
+		if (result.hasErrors()) {
+
+			List<String> errorList = new ArrayList<String>();
+			for (ObjectError error : result.getAllErrors()) {
+				errorList.add(error.getDefaultMessage());
+
+			}
+			model.addAttribute("validationError", errorList);
+			return "user/add";
+
+		}
+		// ユーザー情報の登録
+		userService.create(userRequest);
+		return "redirect:/user/list";
+
 	}
 
 }
