51d7504d219c282cc0d24ad22245fc8f14425a0e
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2026-05-14 11:43:22 +0900

    一覧画面の実装

--- trainingspring1/src/main/java/com/example/demo/controller/UserController.java
+++ trainingspring1/src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.Entity.User;
 import com.example.demo.Service.UserService;
@@ -30,6 +31,30 @@ public class UserController {
 		List<User> userlist = userService.searchAll();
 		model.addAttribute("userlist", userlist);
 		return "user/list";
+	}
+
+	/**
+	 * ユーザー新規登録画面表示
+	 * @param model Model
+	 * @param ユーザー情報一覧画面
+	 */
+
+	@GetMapping(value = "/user/add")
+	public String displayAdd(Model model) {
+		return "user./add";
+	}
+
+	/**
+	 * ユーザー情報詳細画面を表示
+	 * @param id 表示するユーザーID
+	 * @param model Model
+	 * @return ユーザー情報詳細画面
+	 */
+
+	@GetMapping("/user/{id}")
+	public String displayView(@PathVariable Long id, Model model) {
+
+		return "user/view";
 	}
 
 }
